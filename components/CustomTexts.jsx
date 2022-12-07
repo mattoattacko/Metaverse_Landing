b404cba4f4f2439a6d210a630981463c7333f286
unknown <matt.mcquain@gmail.com>
2022-12-06 21:24:27 -0800

    Explore section complete. CustomTexts complete. Error when clicking Hero button as its not scrolling to the correct Explore component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -21,6 +21,13 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-export const TitleText = ({ title, textStyle }) => (
-  <h2>Title Text</h2>
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`${textStyles} mt-[8px] font-bold md:text-[64px] text-[40px] text-white`}
+  >
+    {title}
+  </motion.h2>
 );
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -8,10 +8,34 @@ import { exploreWorlds } from '../constants';
 
 import styles from '../styles';
 
-const Explore = () => (
-  <section>
-    Explore section
-  </section>
-);
+const Explore = () => {
+  const [active, setActive] = useState('world-2');
+
+  return (
+    <section className={`${styles.paddings}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText title={<>Choose the world you desire <br className="md:block hidden" /> to explore</>} textStyles="text-center" />
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
