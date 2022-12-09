8f1ed99423df4481e1b403dc254bf989e7a87d94
unknown <matt.mcquain@gmail.com>
2022-12-08 21:14:55 -0800

    fixed text rendering issue on StartSteps components

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -13,26 +13,35 @@ const GetStarted = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
+      viewport={{ once: 'false', amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
         variants={planetVariants('left')}
-        className={`${styles.flexCenter} flex-1`}
+        className={`flex-1 ${styles.flexCenter}`}
       >
         <img
           src="/get-started.png"
           alt="Get Started"
           className="w-[90%] h-[90%] object-contain"
         />
-        <motion.div
-          variants={fadeIn('left', 'tween', 0.2, 1)}
-          className="flex-[0.75] flex justify-center flex-col"
-        >
-          <TypingText title="| How Metaversus Works" />
-          <TitleText title={<>Get started with just a few clicks</>} />
-        </motion.div>
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="| How Metaversus Works" />
+        <TitleText title={<>Get started with just a few clicks</>} />
 
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature, index) => (
+            <StartSteps
+              key={feature}
+              number={index + 1}
+              text={feature}
+            />
+          ))}
+        </div>
       </motion.div>
     </motion.div>
   </section>
