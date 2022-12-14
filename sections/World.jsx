3b5967382587e38147b68cd9f5bb8a8ea55b69d3
unknown <matt.mcquain@gmail.com>
2022-12-13 20:43:30 -0800

    the World section is complete but still needs to have the extra assets included and styled

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,66 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText, TitleText } from '../components';
+import { staggerContainer, fadeIn } from '../utils/motion';
+
+import styles from '../styles';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People around the World" textStyles="text-center" />
+      <TitleText title={<>Track friends around you and invite them to play together in the same worlds!</>} textStyles="text-center" />
+
+      <motion.div
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        {/* map image */}
+        <img
+          src="/map.png"
+          alt="Map"
+          className="w-full h-full object-cover"
+        />
+
+        {/* The little images of people */}
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="/people-01.png"
+            alt="People"
+            className="w-full h-full z-10"
+          />
+          {/* <img
+            src="/Union.png"
+            alt="Union"
+            className="absolute bottom-21 right-19 w-[100%] h-[100%] p=[6px] -z-1"
+          /> */}
+
+        </div>
+
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="/people-02.png"
+            alt="People"
+            className="w-full h-full"
+          />
+        </div>
+
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="/people-03.png"
+            alt="People"
+            className="w-full h-full"
+          />
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
